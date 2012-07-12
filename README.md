81f21b033ad4a4008eabdbb9946c0ffc4e0e3e55
alesys <alesys@gmail.com>
2012-07-11 20:00:48 -0500

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+learning-nodejs
+===============
+
+Sample nodejs examples
+
+# Mouse Tracket Test by Rolf Ruiz (Alesys.net)
+
+
+Simple mousetracker using socket.io and express to handle socket and http requests; jquery and TweenMax to make smoother animations.
+
---
+++ app.js
@@ -0,0 +1,50 @@
+var app = require('express').createServer();
+var io  = require('socket.io').listen(app);
+var client_id = 0;
+app.listen(5000);
+
+app.get('/', handleExpress);
+io.sockets.on('connection', handleSocketConnect);
+
+function handleExpress(req, res)
+{
+	//console.log('req: '+ req);
+	//console.log('res: ' + res);
+	res.sendfile(__dirname + '/html/index.html');
+}
+
+function handleSocketConnect(socket)
+{
+	console.log('socket conectado');
+	socket.set('client_id', client_id, function(){});
+	socket.emit('onConnect', {id: client_id++});
+
+	// Escuchar
+	socket.on('update', handleUpdate);
+	socket.on('disconnect', handleDisconnect);
+}
+
+function handleUpdate(data)
+{
+	// data debe de tener
+	// {id, nombre, point{} }
+	var socket = this;
+	io.sockets.emit('onUpdate', data);
+}
+
+function handleDisconnect()
+{
+	console.log('socket desconectado');
+	var socket = this;
+	var _client_id;
+	socket.get('client_id',function(err, name){
+		_client_id = name;
+		console.log(name);
+	});
+	console.log('client_id:'+_client_id);
+	
+	io.sockets.emit('clientDisconnected',
+		{
+			id: _client_id
+		});
+}
